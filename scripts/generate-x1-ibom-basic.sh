da9c23cce7a30c579bc550a23070f44bf2148ba6
Richard Goulter <richard.goulter@gmail.com>
2021-12-14 17:59:48 +0700

    Scripts: generate IBOM, and for "basic" variant of X-1: MiniF4-36

--- scripts/generate-x1-ibom-basic.sh
+++ scripts/generate-x1-ibom-basic.sh
@@ -8,11 +8,13 @@ PCB_DIR="${SCRIPTS_DIR}/../pcb"
 
 InteractiveHtmlBom \
     --checkboxes="" \
-    --name-format="ibom-x1-minif4_36-rev%r" \
+    --name-format="ibom-x1-minif4_36-rev%r-basic" \
     --sort-order="R,C,C_,D_,D_BL_,Q,U,J,M,SW_BOOT,SW_RESET,SW_,SW_RE,H,~" \
     --extra-data-file="../pcb/keyboard-100x100-minif4-dual-rgb-reversible.net" \
+    --group-fields="Value,Description" \
     --extra-fields="Description,Comment" \
-    --group-fields="Value" \
     --show-fields="References,Value,Description,Comment,Quantity" \
+    --variant-field="fit_field" \
+    --variants-blacklist="-basic" \
     --no-browser \
     "${PCB_DIR}/keyboard-100x100-minif4-dual-rgb-reversible.kicad_pcb"
