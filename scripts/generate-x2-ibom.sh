ee7c08b19c9a5f857b42d635df5c693cd4a82b1b
Richard Goulter <richard.goulter@gmail.com>
2021-12-14 20:24:53 +0700

    Scripts: add script for generating X-2 interactive BOM

---
+++ scripts/generate-x2-ibom.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash shell-interactive-html-bom.nix
+
+set -ex
+
+SCRIPTS_DIR="$(dirname "$0")"
+PCB_DIR="${SCRIPTS_DIR}/../pcb"
+
+InteractiveHtmlBom \
+    --checkboxes="" \
+    --name-format="ibom-x2-lumberjack-arm-rev%r" \
+    --sort-order="R,C,C_,D_,D_BL_,Q,U,J,M,SW_BOOT,SW_RESET,SW_,SW_RE,H,~" \
+    --extra-data-file="../pcb/keyboard-x2-lumberjack-arm.net" \
+    --extra-fields="Description,Comment" \
+    --group-fields="Value" \
+    --show-fields="References,Value,Description,Comment,Quantity" \
+    --no-browser \
+    "${PCB_DIR}/keyboard-x2-lumberjack-arm.kicad_pcb"
