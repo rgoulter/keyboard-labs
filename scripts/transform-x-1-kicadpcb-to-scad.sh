d3850cef0534db30c4ead3c24f8468485b34038f
Richard Goulter <richard.goulter@gmail.com>
2021-12-16 14:18:45 +0700

    Scripts: convenience scripts for running transform_kicadpcb_scad

---
+++ scripts/transform-x-1-kicadpcb-to-scad.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#!nix-shell -i bash
+
+set -ex
+
+SCRIPTS_DIR="$(dirname "$0")"
+PCB_DIR="${SCRIPTS_DIR}/../pcb"
+
+python transform_kicadpcb_to_scad.py \
+    --kicad_pcb_path "${PCB_DIR}/keyboard-100x100-minif4-dual-rgb-reversible.kicad_pcb"
+
