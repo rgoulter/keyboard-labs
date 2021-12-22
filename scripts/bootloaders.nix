36113e7dc6caa300a366348b2605025a014a7981
Richard Goulter <richard.goulter@gmail.com>
2021-12-22 14:12:24 +0700

    Scripts: write nix derivation for fetching STM32F401's tinyuf2 bootloader

--- scripts/bootloaders.nix
+++ scripts/bootloaders.nix
@@ -30,4 +30,38 @@
       '';
     };
   };
+  stm32f401 = {
+    tinyuf2 = pkgs.stdenv.mkDerivation rec {
+      pname = "tinyuf2";
+      version = "0.6.2";
+
+      # tinyuf2 assumes compiled using git repo.
+      # Takes about 7m to clone all the subrepos(!!).
+      # But, if set fetchSubmodules = false, then
+      #  the repo otherwise complains. and git doesn't recognise
+      #  the submodule pathspec in the build phase.
+      src = pkgs.fetchgit {
+        url = "https://github.com/adafruit/tinyuf2";
+        rev = version;
+        sha256 = "0irgpf7zbbr22ji7hibcyz6hvw9l980sxir3xyim7k71l2rwpgxh";
+      };
+
+      buildInputs = with pkgs; [
+        gcc-arm-embedded
+        git
+        gnumake
+      ];
+
+      buildPhase = ''
+        ls -al
+        cd ports/stm32f4
+        make BOARD=stm32f401_blackpill all
+      '';
+
+      installPhase = ''
+        mkdir -p $out/
+        cp _build/stm32f401_blackpill/tinyuf2-stm32f401_blackpill* $out/
+      '';
+    };
+  };
 }
