536dc9ade113a4ef3897329957847e8c8d58acb0
Richard Goulter <richard.goulter@gmail.com>
2023-08-03 10:37:07 +0700

    iboms: add netlist to x2 iboms

--- scripts/generate-x2-hsrgb-ibom.sh
+++ scripts/generate-x2-hsrgb-ibom.sh
@@ -12,9 +12,10 @@ InteractiveHtmlBom \
   --checkboxes="" \
   --name-format="ibom-${BOARD}-rev%r" \
   --sort-order="R,C,C_,D_,D_BL_,Q,U,J,M,SW_BOOT,SW_RESET,SW_,SW_RE,H,~" \
-  --blacklist="HOLE" \
+  --extra-data-file="../pcb/${BOARD}.net" \
   --extra-fields="Description,Comment" \
   --group-fields="Value" \
   --show-fields="References,Value,Description,Comment,Quantity" \
   --no-browser \
+  --blacklist="HOLE,H5" \
   "${PCB_DIR}/${BOARD}.kicad_pcb"
