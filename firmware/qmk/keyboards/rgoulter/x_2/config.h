7fc51feaacc782291fe1f9230b7715f254c64fba
Richard Goulter <richard.goulter@gmail.com>
2023-08-03 10:10:27 +0700

    firmware/qmk: sync

--- firmware/qmk/keyboards/rgoulter/x_2/config.h
+++ firmware/qmk/keyboards/rgoulter/x_2/config.h
@@ -26,6 +26,13 @@
 
 #ifdef RGB_MATRIX_ENABLE
 #define DRIVER_LED_TOTAL 70
+#define RGBLED_NUM 70
 #define RGB_MATRIX_LED_COUNT 70
 #define RGB_MATRIX_KEYPRESSES
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#ifdef RGBLIGHT_ENABLE
+#error "rgblight and rgb_matrix both enabled"
+#endif
 #endif
