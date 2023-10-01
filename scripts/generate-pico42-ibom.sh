c5c9ab7933051adced03647f6c3ab8ceb37612fe
Richard Goulter <richard.goulter@gmail.com>
2023-10-01 15:28:14 +0700

    scripts: add script to generate IBOM for Pico42

---
+++ scripts/generate-pico42-ibom.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash ../pcb/shell.nix
+# shellcheck shell=bash
+
+set -ex
+
+SCRIPTS_DIR="$(dirname "$0")"
+PCB_DIR="${SCRIPTS_DIR}/../pcb"
+BOARD="keyboard-pico42"
+
+InteractiveHtmlBom \
+  --checkboxes="" \
+  --name-format="ibom-${BOARD}-rev%r" \
+  --sort-order="R,C,C_,D_,D_BL_,Q,U,J,M,SW_BOOT,SW_RESET,SW_,SW_RE,H,~" \
+  --extra-data-file="../pcb/${BOARD}.net" \
+  --extra-fields="Description,Comment" \
+  --group-fields="Value" \
+  --show-fields="References,Value,Description,Comment,Quantity" \
+  --no-browser \
+  "${PCB_DIR}/${BOARD}.kicad_pcb"
