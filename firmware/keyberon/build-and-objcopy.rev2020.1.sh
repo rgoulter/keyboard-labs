60f5baefc25b583cb1cd7e55187e671d0c343a59
Richard Goulter <richard.goulter@gmail.com>
2023-10-06 12:02:46 +0700

    firmware: keyberon: add Makefile

--- firmware/keyberon/build-and-objcopy.rev2020.1.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-cargo build --bin minif4-36-rev2020_1-lhs --release
-cargo build --bin minif4-36-rev2020_1-rhs --release
--- firmware/keyberon/build-and-objcopy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-cargo build --bin minif4-36-rev2021_1-lhs --release
-cargo build --bin minif4-36-rev2021_1-rhs --release
-
-cargo objcopy --bin minif4-36-rev2021_1-lhs --release -- -O binary keyberon-left.bin
-cargo objcopy --bin minif4-36-rev2021_1-rhs --release -- -O binary keyberon-right.bin
