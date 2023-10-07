082e8f2818295ef4be90d88937b16caa9d4cc308
Richard Goulter <richard.goulter@gmail.com>
2023-10-07 16:03:52 +0700

    firmware: keyberon: refactor layouts/common to export macros

--- firmware/keyberon/src/layouts/common.rs
+++ firmware/keyberon/src/layouts/common.rs
@@ -1,58 +1,69 @@
 /// Macro for "shift + key".
+#[macro_export]
 macro_rules! sk {
     ($k:ident) => {
-        m(&[LShift, $k].as_slice())
+        keyberon::action::m(&[keyberon::key_code::KeyCode::LShift, $k].as_slice())
     };
 }
 
 /// Macro for "tap-hold, with super modifier".
+#[macro_export]
 macro_rules! s {
     ($k:ident) => {
-        HoldTap(&HoldTapAction {
+        keyberon::action::Action::HoldTap(&keyberon::action::HoldTapAction {
             timeout: 200,
             tap_hold_interval: 0,
-            config: HoldTapConfig::Default,
-            hold: k(LShift),
-            tap: k($k),
+            config: keyberon::action::HoldTapConfig::Default,
+            hold: keyberon::action::k(keyberon::key_code::KeyCode::LShift),
+            tap: keyberon::action::k($k),
         })
     };
 }
 
 /// Macro for "tap-hold, with ctrl modifier".
+#[macro_export]
 macro_rules! c {
     ($k:ident) => {
-        HoldTap(&HoldTapAction {
+        keyberon::action::Action::HoldTap(&keyberon::action::HoldTapAction {
             timeout: 200,
             tap_hold_interval: 0,
-            config: HoldTapConfig::Default,
-            hold: k(LCtrl),
-            tap: k($k),
+            config: keyberon::action::HoldTapConfig::Default,
+            hold: keyberon::action::k(keyberon::key_code::KeyCode::LCtrl),
+            tap: keyberon::action::k($k),
         })
     };
 }
 
 /// Macro for "tap-hold, with gui modifier".
+#[macro_export]
 macro_rules! g {
     ($k:ident) => {
-        HoldTap(&HoldTapAction {
+        keyberon::action::Action::HoldTap(&keyberon::action::HoldTapAction {
             timeout: 200,
             tap_hold_interval: 0,
-            config: HoldTapConfig::Default,
-            hold: k(LGui),
-            tap: k($k),
+            config: keyberon::action::HoldTapConfig::Default,
+            hold: keyberon::action::k(keyberon::key_code::KeyCode::LGui),
+            tap: keyberon::action::k($k),
         })
     };
 }
 
 /// Macro for "tap-hold, with alt modifier".
+#[macro_export]
 macro_rules! a {
     ($k:ident) => {
-        HoldTap(&HoldTapAction {
+        keyberon::action::Action::HoldTap(&keyberon::action::HoldTapAction {
             timeout: 200,
             tap_hold_interval: 0,
-            config: HoldTapConfig::Default,
-            hold: k(LAlt),
-            tap: k($k),
+            config: keyberon::action::HoldTapConfig::Default,
+            hold: keyberon::action::k(keyberon::key_code::KeyCode::LAlt),
+            tap: keyberon::action::k($k),
         })
     };
 }
+
+pub use sk;
+pub use s;
+pub use c;
+pub use g;
+pub use a;
