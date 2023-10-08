8e9e33896c6757e93c14fd22fb60319527844971
Richard Goulter <richard.goulter@gmail.com>
2023-10-08 16:11:25 +0700

    firmware: keyberon: update UsbClass to use usbd-h-i-d

--- firmware/keyberon/src/common.rs
+++ firmware/keyberon/src/common.rs
@@ -3,7 +3,11 @@ use stm32f4xx_hal::otg_fs::UsbBusType;
 use keyberon::layout::Event;
 use usb_device::class::UsbClass as _;
 
-pub type UsbClass = keyberon::hid::HidClass<'static, UsbBusType, keyberon::keyboard::Keyboard<()>>;
+use frunk::HList;
+use usbd_human_interface_device::device::keyboard::NKROBootKeyboard;
+use usbd_human_interface_device::usb_class::UsbHidClass;
+
+pub type UsbClass = UsbHidClass<'static, UsbBusType, HList!(NKROBootKeyboard<'static, UsbBusType>)>;
 
 pub type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
 
