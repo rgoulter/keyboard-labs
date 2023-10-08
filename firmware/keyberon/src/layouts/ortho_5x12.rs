7b8efd53e3385e6fad12e17a951052e37a5be008
Richard Goulter <richard.goulter@gmail.com>
2023-10-08 18:18:20 +0700

    firmware: keyberon: update layout ortho_5x12 to usbd-h-i-d

--- firmware/keyberon/src/layouts/ortho_5x12.rs
+++ firmware/keyberon/src/layouts/ortho_5x12.rs
@@ -1,13 +1,12 @@
-use keyberon::action::{d, k, l, Action::*, HoldTapAction, HoldTapConfig};
+use keyberon::action::{Action::*, HoldTapConfig, d, k, l};
 use keyberon::chording::ChordDef;
-use keyberon::key_code::KeyCode::*;
+use usbd_human_interface_device::page::{Keyboard::*, Keyboard};
 
 use crate::layouts::actions::{
     LINUX_DESKTOP_LEFT,
     LINUX_DESKTOP_RIGHT,
 };
-
-type Action = keyberon::action::Action<()>;
+use crate::layouts::common::{Action, HoldTapAction};
 
 // const CUT: Action = m(&[LShift, Delete]);
 // const COPY: Action = m(&[LCtrl, Insert]);
@@ -49,7 +48,7 @@ const RS2_BSP: Action = HoldTap(&HoldTapAction {
     config: HoldTapConfig::Default,
     tap_hold_interval: 0,
     hold: l(RAISE2),
-    tap:  k(BSpace),
+    tap:  k(DeleteBackspace),
 });
 
 const RSE_ENT: Action = HoldTap(&HoldTapAction {
@@ -57,7 +56,7 @@ const RSE_ENT: Action = HoldTap(&HoldTapAction {
     config: HoldTapConfig::Default,
     tap_hold_interval: 0,
     hold: l(RAISE),
-    tap:  k(Enter),
+    tap:  k(ReturnEnter),
 });
 
 const LWR_ESC: Action = HoldTap(&HoldTapAction {
@@ -77,112 +76,134 @@ pub const NUM_LAYERS: usize = 11;
 // Rows = 5 physical + 1 virtual for chords
 // Layers = 11
 // Custom action type = ()
-pub type Layers = keyberon::layout::Layers<COLS, ROWS_AND_MACROS, NUM_LAYERS, ()>;
-pub type Layout = keyberon::layout::Layout<COLS, ROWS_AND_MACROS, NUM_LAYERS, ()>;
+pub type Layers = keyberon::layout::Layers<COLS, ROWS_AND_MACROS, NUM_LAYERS, (), Keyboard>;
+pub type Layout = keyberon::layout::Layout<COLS, ROWS_AND_MACROS, NUM_LAYERS, (), Keyboard>;
+
+const _______: Action = keyberon::action::Action::Trans;
 
 #[rustfmt::skip]
-pub static LAYERS: Layers = keyberon::layout::layout! {
+pub static LAYERS: Layers = [
     // 0: Dvorak
-    {
-        [ 1          2         3       4        5        Grave    BSpace 6        7        8      9      0     ],
-        [{k(Quote) } ,         .       P        Y       {NoOp}   {NoOp}  F        G        C      R      L     ],
-        [{a!(A)    }{g!(O)   }{c!(E) }{s!(U)}   I       {NoOp}   {NoOp}  D       {s!(H)  }{c!(T)}{g!(N)}{a!(S)}],
-        [ ;          Q         J       K        X       {NoOp}   {NoOp}  B        M        W      V      Z     ],
-        [{l(NUMPAD)}{l(FN)}    t      {LWR_TAB}{LW2_ESC} Space    Space {RS2_BSP}{RSE_ENT} t      t      t     ],
-        [{LINUX_DESKTOP_LEFT}{LINUX_DESKTOP_RIGHT}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [k(Keyboard1), k(Keyboard2), k(Keyboard3), k(Keyboard4), k(Keyboard5), k(Grave), k(DeleteBackspace), k(Keyboard6), k(Keyboard7), k(Keyboard8), k(Keyboard9), k(Keyboard0)],
+        [k(Apostrophe), k(Comma), k(Dot), k(P), k(Y), NoOp, NoOp, k(F), k(G), k(C), k(R), k(L)],
+        [a!(A), g!(O), c!(E), s!(U), k(I), NoOp, NoOp, k(D),  s!(H), c!(T), g!(N), a!(S)],
+        [k(Semicolon), k(Q), k(J), k(K), k(X), NoOp, NoOp, k(B), k(M), k(W), k(V), k(Z)],
+        [l(NUMPAD), l(FN), NoOp, LWR_TAB, LW2_ESC, k(Space), k(Space), RS2_BSP, RSE_ENT, NoOp, NoOp, NoOp],
+        // Combos:
+        [LINUX_DESKTOP_LEFT, LINUX_DESKTOP_RIGHT, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 1: QWERTY
-    {
-        [ 1          2         3       4        5        Grave     BSpace 6        7        8      9      0    ],
-        [ Q          W         E       R        T        Tab       '['    Y        U        I      O      P    ],
-        [ A          S         D       F        G        Escape    '\''   H        J        K      L      ;    ],
-        [ Z          X         C       V        B        LShift   {NoOp}  N        M        ,      .      /    ],
-        [{l(NUMPAD)} t         t      {LWR_TAB}{LW2_ESC} Space     Space {RS2_BSP}{RSE_ENT} t      t      t    ],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [k(Keyboard1), k(Keyboard2), k(Keyboard3), k(Keyboard4), k(Keyboard5), k(Grave), k(DeleteBackspace), k(Keyboard6), k(Keyboard7), k(Keyboard8), k(Keyboard9), k(Keyboard0)],
+        [k(Q), k(W), k(E), k(R), k(T), k(Tab), k(LeftBrace), k(Y), k(U), k(I), k(O), k(P)],
+        [k(A), k(S), k(D), k(F), k(G), k(Escape), k(Apostrophe), k(H), k(J), k(K), k(L), k(Semicolon)],
+        [k(Z), k(X), k(C), k(V), k(B), k(LeftShift), NoOp, k(N), k(M), k(Comma), k(Dot), k(ForwardSlash)],
+        [l(NUMPAD), NoOp, NoOp, LWR_TAB, LW2_ESC, k(Space), k(Space), RS2_BSP, RSE_ENT, NoOp, NoOp, NoOp],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 2: Gaming
-    {
-        [Grave     1         2         3       4        5        6        7          8      9      0    BSpace],
-        [Tab       Q         W         E       R        T        Y        U          I      O      P    '[' ],
-        [CapsLock  A         S         D       F        G        H        J          K      L      ;    '\''],
-        [LShift    Z         X         C       V        B        N        M          ,      .      /    '\\'],
-        [LCtrl     LGui      LAlt      Tab    {LWR_ESC} Space    BSpace  {RSE_ENT}   Left   Down   Up   Right],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [k(Grave), k(Keyboard1), k(Keyboard2), k(Keyboard3), k(Keyboard4), k(Keyboard5), k(Keyboard6), k(Keyboard7), k(Keyboard8), k(Keyboard9), k(Keyboard0), k(DeleteBackspace)],
+        [k(Tab), k(Q), k(W), k(E), k(R), k(T), k(Y), k(U), k(I), k(O), k(P), k(LeftBrace)],
+        [k(CapsLock), k(A), k(S), k(D), k(F), k(G), k(H), k(J), k(K), k(L), k(Semicolon), k(Apostrophe)],
+        [k(LeftShift), k(Z), k(X), k(C), k(V), k(B), k(N), k(M), k(Comma), k(Dot), k(ForwardSlash), k(Backslash)],
+        [k(LeftControl), k(LeftGUI), k(LeftAlt), k(Tab), LWR_ESC, k(Space), k(DeleteBackspace), RSE_ENT, k(LeftArrow), k(DownArrow), k(UpArrow), k(RightArrow)],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 3: Lower
-    {
-        [ t           t         t         t         t            t               t            t         t           t           t           t           ],
-        [{sk!(Kb1)  }{sk!(Kb2)}{sk!(Kb3)}{sk!(Kb4)}{sk!(Kb5)   }{sk!(Grave)}    {sk!(Bslash)}{sk!(Kb6)}{sk!(Kb7)  }{sk!(Kb8)  }{sk!(Kb9)  }{sk!(Kb0)   }],
-        [{sk!(Grave)} t         t         t         t            Insert         {sk!(Slash) } Insert   {sk!(Minus)}{sk!(Equal)} '{'         '}'         ],
-        [{sk!(Slash)} t         t         t        {sk!(Bslash)} t               t            t         t           t          {sk!(Slash)}{sk!(Bslash)}],
-        [ t           t         t         t         t            t               t            t        {l(ADJUST)}  t           t           t           ],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [_______,           _______,        _______,        _______,        _______,        _______,    _______,              _______,        _______, _______, _______,      _______],
+        [sk!(Keyboard1),    sk!(Keyboard2), sk!(Keyboard3), sk!(Keyboard4), sk!(Keyboard5), sk!(Grave), sk!(DeleteBackspace), sk!(Keyboard6), sk!(Keyboard7), sk!(Keyboard8), sk!(Keyboard9),    sk!(Keyboard0)],
+        [sk!(Grave),        _______,        _______,        _______,        _______,        k(Insert),  sk!(ForwardSlash),           k(Insert),      sk!(Minus),      sk!(Equal),    sk!(LeftBrace),    sk!(RightBrace)],
+        [sk!(ForwardSlash), _______,        _______,        _______,        sk!(Backslash), _______,     _______,             _______,        _______,         _______,       sk!(ForwardSlash), sk!(Backslash)],
+        [_______,           _______,        _______,        _______,        _______,        _______,     _______,             _______,        l(ADJUST),       _______,       _______,           _______],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 4: Lower2
-    {
-        [t   t  t  t  t          t    t t    t      t    t     t],
-        [F12 F7 F8 F9 PScreen    t    t t    t      t    t     t],
-        [F11 F4 F5 F6 ScrollLock t    t Left Down   Up   Right t],
-        [F10 F1 F2 F3 Pause      t    t Home PgDown PgUp End   t],
-        [t   t  t  t  t          t    t t    t      t    t     t],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [_______, _______, _______, _______, _______,        _______, _______, _______,      _______,      _______,    _______, _______],
+        [k(F12),  k(F7),   k(F8),   k(F9),   k(PrintScreen), _______, _______, _______,      _______,      _______,    _______, _______],
+        [k(F11),  k(F4),   k(F5),   k(F6),   k(ScrollLock),  _______, _______, k(LeftArrow), k(DownArrow), k(UpArrow), k(RightArrow), _______],
+        [k(F10),  k(F1),   k(F2),   k(F3),   k(Pause),       _______, _______, k(Home),      k(PageDown),  k(PageUp),  k(End),        _______],
+        [_______, _______, _______, _______, _______,        _______, _______, _______,      _______,      _______,    _______, _______],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
-    // 5: Raise
-    {
-        [t     t t  t          t      t         t      t      t     t     t     t     ],
-        [1     2 3  4          5      Grave     BSpace 6      7     8     9     0     ],
-        [Grave t t  t          t      Delete    t      Delete Minus Equal '['   ']'   ],
-        [Slash t t  t          Bslash t         t      t      t     t     Slash Bslash],
-        [t     t t {l(ADJUST)} t      t         t      t      t     t     t     t     ],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    // // 5: Raise
+    [
+        [_______,         _______,      _______,      _______,      _______,      _______,   _______,            _______,      _______, _______, _______,      _______],
+        [k(Keyboard1),    k(Keyboard2), k(Keyboard3), k(Keyboard4), k(Keyboard5), k(Grave),  k(DeleteForward), k(Keyboard6), k(Keyboard7), k(Keyboard8), k(Keyboard9),    k(Keyboard0)],
+        [k(Grave),        _______,      _______,      _______,      _______,      k(DeleteForward), k(ForwardSlash),           k(DeleteForward),    k(Minus),     k(Equal),     k(LeftBrace),    k(RightBrace)],
+        [k(ForwardSlash), _______,      _______,      _______,      k(Backslash), _______,   _______,            _______,      _______,      _______,      k(ForwardSlash), k(Backslash)],
+        [_______,         _______,      _______,      _______,      _______,      _______,   _______,            _______,      l(ADJUST),    _______,      _______,         _______],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 6: Raise2
     // TBI: mouse movement
-    {
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 7: Childproof
-    {
-        [{NoOp    } {NoOp} {NoOp} {NoOp} {NoOp} {NoOp}    {NoOp} {NoOp} {NoOp} {NoOp} {NoOp} {NoOp    }],
-        [{NoOp    } {NoOp} {NoOp} {NoOp} {NoOp} {NoOp}    {NoOp} {NoOp} {NoOp} {NoOp} {NoOp} {NoOp    }],
-        [{NoOp    } {NoOp} {NoOp} {NoOp} {NoOp} {NoOp}    {NoOp} {NoOp} {NoOp} {NoOp} {NoOp} {NoOp    }],
-        [{NoOp    } {NoOp} {NoOp} {NoOp} {NoOp} {NoOp}    {NoOp} {NoOp} {NoOp} {NoOp} {NoOp} {NoOp    }],
-        [{l(LOWER)} {NoOp} {NoOp} {NoOp} {NoOp} {NoOp}    {NoOp} {NoOp} {NoOp} {NoOp} {NoOp} {l(RAISE)}],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [NoOp,     NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp, NoOp, NoOp, NoOp    ],
+        [NoOp,     NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp, NoOp, NoOp, NoOp    ],
+        [NoOp,     NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp, NoOp, NoOp, NoOp    ],
+        [NoOp,     NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp, NoOp, NoOp, NoOp    ],
+        [l(LOWER), NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp, NoOp, NoOp, l(RAISE)],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
     // 8: Numpad
     // Swap the macro on left/right.
-    {
-        [t       t   t   t   t t    t t t t t t],
-        [NumLock Kp7 Kp8 Kp9 t t    t t t t t t],
-        [t       Kp4 Kp5 Kp6 t t    t t t t t t],
-        [t       Kp1 Kp2 Kp3 t t    t t t t t t],
-        [t       Kp0 Kp0 .   t t    t t t t t t],
-        [{LINUX_DESKTOP_RIGHT}{LINUX_DESKTOP_LEFT}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [k(KeypadNumLockAndClear), k(Keypad7), k(Keypad8), k(Keypad9), _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______,    k(Keypad4), k(Keypad5), k(Keypad6), _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______,    k(Keypad1), k(Keypad2), k(Keypad3), _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______,    k(Keypad0), k(Keypad0), k(KeypadDot), _______, _______, _______, _______, _______, _______, _______, _______],
+        // Combos:
+        [LINUX_DESKTOP_RIGHT, LINUX_DESKTOP_LEFT, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
 
-    // 9: Adjust
-    {
-        [t        t t t t t    t t t t t t],
-        [t        t t t t t    t t t t t t],
-        [CapsLock t t t t t    t t {d(BASE_QWERTY)}{d(BASE_GAMING)}{d(BASE_DSK)}{d(CHILDPROOF)}],
-        [t        t t t t t    t t t t t t],
-        [t        t t t t t    t t t t t t],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    // // 9: Adjust
+    // {
+    //     [t        t t t t t    t t t t t t],
+    //     [t        t t t t t    t t t t t t],
+    //     [CapsLock t t t t t    t t {d(BASE_QWERTY)}{d(BASE_GAMING)}{d(BASE_DSK)}{d(CHILDPROOF)}],
+    //     [t        t t t t t    t t t t t t],
+    //     [t        t t t t t    t t t t t t],
+    //     [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
+    // }
+    [
+        [NoOp,        NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp,           NoOp,           NoOp,        NoOp],
+        [NoOp,        NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp,           NoOp,           NoOp,        NoOp],
+        [k(CapsLock), NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, d(BASE_QWERTY), d(BASE_GAMING), d(BASE_DSK), d(CHILDPROOF)],
+        [NoOp,        NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp,           NoOp,           NoOp,        NoOp],
+        [NoOp,        NoOp, NoOp, NoOp, NoOp, NoOp,    NoOp, NoOp, NoOp,           NoOp,           NoOp,        NoOp],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
+
+    // c.f. QMK code:
     // Pinky-outer-column
     // [_ADJUST] = LAYOUT(
     //     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       \
@@ -193,14 +214,16 @@ pub static LAYERS: Layers = keyberon::layout::layout! {
     // ),
 
     // 10: Fn
-    {
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [t t t t t t    t t t t t t],
-        [{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}  {NoOp}{NoOp}{NoOp}{NoOp}{NoOp}{NoOp}],
-    }
+    [
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______],
+        // Combos:
+        [NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp, NoOp],
+    ],
+
     // Same as default layer
     // [_FN] = LAYOUT(
     //     _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
@@ -209,7 +232,7 @@ pub static LAYERS: Layers = keyberon::layout::layout! {
     //     _______, _______, _______, _______, _______, _______,    _______, _______, RGB_TOG, RGB_HUD, RGB_HUI, RGB_MOD,
     //     _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______
     // )
-};
+];
 
 pub const CHORDS: [ChordDef; 2] = [
     ((5, 0), &[(3, 2), (3, 3)]), // JK -> Desktop Left
