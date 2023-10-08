23a11f57d528b4abe12ec2af15e29d5e95433a74
Richard Goulter <richard.goulter@gmail.com>
2023-10-08 16:32:50 +0700

    firmware: keyberon: update actions to usbd-h-i-d

--- firmware/keyberon/src/layouts/actions.rs
+++ firmware/keyberon/src/layouts/actions.rs
@@ -1,15 +1,14 @@
-use keyberon::action::m;
-use keyberon::key_code::KeyCode::*;
+use usbd_human_interface_device::page::Keyboard::*;
 
-type Action = keyberon::action::Action<()>;
+use crate::layouts::common::Action;
 
 // // macOS
 // #define CODE16_MACOS_DESKTOP_LEFT  LCTL(KC_LEFT)
 // #define CODE16_MACOS_DESKTOP_RIGHT LCTL(KC_RIGHT)
 
 // // Linux, Gnome shell
-pub const LINUX_DESKTOP_LEFT : Action = m(&[LCtrl, LAlt, Left].as_slice());
-pub const LINUX_DESKTOP_RIGHT: Action = m(&[LCtrl, LAlt, Right].as_slice());
+pub const LINUX_DESKTOP_LEFT : Action = Action::MultipleKeyCodes(&[LeftControl, LeftAlt, LeftArrow].as_slice());
+pub const LINUX_DESKTOP_RIGHT: Action = Action::MultipleKeyCodes(&[LeftControl, LeftAlt, RightArrow].as_slice());
 //
 // // Windows 10
 // #define CODE16_WIN_DESKTOP_LEFT  LCTL(LGUI(KC_LEFT))
