2ad102da5fd382f2ec136385225dbedd02fc3f37
Richard Goulter <richard.goulter@gmail.com>
2023-10-11 11:53:11 +0700

    firmware: keyberon: flake.nix: update with rp2040

--- firmware/keyberon/flake.nix
+++ firmware/keyberon/flake.nix
@@ -28,6 +28,7 @@
           default.rustfmt
           default.cargo
           default.rustc
+          targets."thumbv6m-none-eabi".latest.rust-std
           targets."thumbv7em-none-eabihf".latest.rust-std
         ];
     in let
@@ -51,6 +52,9 @@
       };
 
       packages = let
+        rp2040-bins = [
+          "blinky"
+        ];
         stm32f4-bins = [
           "minif4-36-rev2021_4-lhs"
           "minif4-36-rev2021_4-rhs"
@@ -60,6 +64,25 @@
           "stm32f4-onekey"
         ];
         in rec {
+        keyberon-firmware-rp2040-elf =
+          let
+            target = "thumbv6m-none-eabi";
+          in
+          (naersk.lib.${system}.override {
+            cargo = toolchain;
+            rustc = toolchain;
+          })
+          .buildPackage {
+            src = ./.;
+            overrideMain = x: {
+              preConfigure = ''
+                cargo_build_options="$cargo_build_options --package=keyboard-labs-keyberon-rp2040"
+              '';
+            };
+            CARGO_BUILD_TARGET = target;
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
+          };
+
         keyberon-firmware-stm32f4-elf =
           let
             target = "thumbv7em-none-eabihf";
@@ -86,8 +109,10 @@
           env PATH=$PATH:${pkgs.cargo-binutils}/bin \
             ${pkgs.gnumake}/bin/make \
               --file ${./Makefile} \
+              RP2040_RELEASE_TARGET_DIR=${keyberon-firmware-rp2040-elf}/bin \
               STM32F4_RELEASE_TARGET_DIR=${keyberon-firmware-stm32f4-elf}/bin \
               DEST_DIR=$out/bin \
+              ${nixpkgs.lib.concatStringsSep " " (map (x: "$out/bin/${x}.bin") rp2040-bins)} \
               ${nixpkgs.lib.concatStringsSep " " (map (x: "$out/bin/${x}.bin") stm32f4-bins)}
         '';
 
@@ -99,8 +124,10 @@
           env PATH=$PATH:${uf2conv}/bin \
             ${pkgs.gnumake}/bin/make \
               --file ${./Makefile} \
+              RP2040_RELEASE_TARGET_DIR=${keyberon-firmware-rp2040-elf}/bin \
               STM32F4_RELEASE_TARGET_DIR=${keyberon-firmware-stm32f4-elf}/bin \
               DEST_DIR=$out/bin \
+              ${nixpkgs.lib.concatStringsSep " " (map (x: "$out/bin/${x}.uf2") rp2040-bins)} \
               ${nixpkgs.lib.concatStringsSep " " (map (x: "$out/bin/${x}.uf2") stm32f4-bins)}
 
           rm $out/bin/*.bin
