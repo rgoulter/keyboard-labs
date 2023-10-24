d7adbf6389e9c33b6dec93077f8238ea9d97d081
Richard Goulter <richard.goulter@gmail.com>
2023-10-24 22:16:37 +0700

    firmware/vial: sync

---
+++ firmware/vial/keyboards/rgoulter/pico42/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7D, 0xA6, 0xD6, 0xDB, 0xF6, 0xA6, 0xA7, 0x55}
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
