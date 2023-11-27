4ef763b65f18039791b1f9d8d3737bf0ffa574dd
Richard Goulter <richard.goulter@gmail.com>
2023-11-27 23:21:31 +0700

    nix: add wchisp package

---
+++ nix/pkgs/wchisp/default.nix
@@ -0,0 +1,13 @@
+{ lib, fetchFromGitHub, rustPlatform }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "wchisp";
+  version = "0.3-git";
+  src = fetchFromGitHub {
+    owner = "ch32-rs";
+    repo = pname;
+    rev = "4b4787243ef9bc87cbbb0d95c7482b4f7c9838f1";
+    hash = "sha256-Ju2DBv3R4O48o8Fk/AFXOBIsvGMK9hJ8Ogxk47f7gcU=";
+  };
+  cargoHash = "sha256-MbCLQBk7aJXcUOalsNbSxozvI2Tx3kerLXgi4l7BKoc=";
+}
