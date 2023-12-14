8e292370ac03f03cb1e8f41a947f49b0b0542180
Richard Goulter <richard.goulter@gmail.com>
2023-12-14 11:58:05 +0700

    firmware/vial: sync

---
+++ firmware/vial/keyboards/rgoulter/pykey40/lite/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAB, 0xAC, 0x7A, 0xFB, 0x1C, 0x7B, 0x43, 0x95}
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
