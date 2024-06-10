be37e1c26df632c51e96ddc6d58d22de28a89353
Richard Goulter <richard.goulter@gmail.com>
2024-06-10 23:12:51 +0700

    keyberon: flake.nix: refactor to flake-parts

--- firmware/keyberon/flake.nix
+++ firmware/keyberon/flake.nix
@@ -4,7 +4,7 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     naersk = {
       url = "github:nmattia/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,97 +13,101 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     fenix,
-    flake-utils,
+    flake-parts,
     naersk,
     nixpkgs,
     systems,
   }:
-    flake-utils.lib.eachSystem (import systems) (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      toolchain = with fenix.packages.${system};
-        combine [
-          complete.clippy
-          complete.llvm-tools-preview
-          complete.rust-src
-          default.rustfmt
-          default.cargo
-          default.rustc
-          targets."thumbv6m-none-eabi".latest.rust-std
-          targets."thumbv7em-none-eabihf".latest.rust-std
-        ];
-    in let
-      uf2conv = pkgs.callPackage ../../nix/pkgs/uf2conv {};
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.cargo-binutils
-          pkgs.elf2uf2-rs
-          pkgs.hidrd
-          pkgs.just
-          pkgs.rust-analyzer
-          pkgs.usbutils
-          pkgs.probe-rs
-          pkgs.stlink
-          toolchain
-          uf2conv
-        ];
-        RUSTC = "${toolchain}/bin/rustc";
-        RUST_SRC_PATH = "${toolchain}/lib/rustlib/src";
-      };
-
-      packages = let
-        rp2040-bins = [
-          "weact-pico-onekey"
-        ];
-        stm32f4-bins = [
-          "minif4-36-rev2021_4-lhs"
-          "minif4-36-rev2021_4-rhs"
-          "minif4-36-rev2021_5-lhs"
-          "minif4-36-rev2021_5-rhs"
-          "x_2-rev2021_1"
-          "weact-minif4-onekey"
-        ];
-      in rec {
-        keyberon-firmware-rp2040-elf = let
-          target = "thumbv6m-none-eabi";
-        in
-          (naersk.lib.${system}.override {
-            cargo = toolchain;
-            rustc = toolchain;
-          })
-          .buildPackage {
-            src = ./.;
-            overrideMain = x: {
-              preConfigure = ''
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: let
+        toolchain = with fenix.packages.${system};
+          combine [
+            complete.clippy
+            complete.llvm-tools-preview
+            complete.rust-src
+            default.rustfmt
+            default.cargo
+            default.rustc
+            targets."thumbv6m-none-eabi".latest.rust-std
+            targets."thumbv7em-none-eabihf".latest.rust-std
+          ];
+        uf2conv = pkgs.callPackage ../../nix/pkgs/uf2conv {};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.cargo-binutils
+            pkgs.elf2uf2-rs
+            pkgs.hidrd
+            pkgs.just
+            pkgs.rust-analyzer
+            pkgs.usbutils
+            pkgs.probe-rs
+            pkgs.stlink
+            toolchain
+            uf2conv
+          ];
+          RUSTC = "${toolchain}/bin/rustc";
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src";
+        };
+        packages = let
+          rp2040-bins = [
+            "weact-pico-onekey"
+          ];
+          stm32f4-bins = [
+            "minif4-36-rev2021_4-lhs"
+            "minif4-36-rev2021_4-rhs"
+            "minif4-36-rev2021_5-lhs"
+            "minif4-36-rev2021_5-rhs"
+            "x_2-rev2021_1"
+            "weact-minif4-onekey"
+          ];
+        in rec {
+          keyberon-firmware-rp2040-elf = let
+            target = "thumbv6m-none-eabi";
+          in
+            (naersk.lib.${system}.override {
+              cargo = toolchain;
+              rustc = toolchain;
+            })
+              .buildPackage {
+                src = ./.;
+                overrideMain = x: {
+                  preConfigure = ''
                 cargo_build_options="$cargo_build_options --package=keyboard-labs-keyberon-rp2040"
               '';
-            };
-            CARGO_BUILD_TARGET = target;
-            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
-          };
+                };
+                CARGO_BUILD_TARGET = target;
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
+              };
 
-        keyberon-firmware-stm32f4-elf = let
-          target = "thumbv7em-none-eabihf";
-        in
-          (naersk.lib.${system}.override {
-            cargo = toolchain;
-            rustc = toolchain;
-          })
-          .buildPackage {
-            src = ./.;
-            overrideMain = x: {
-              preConfigure = ''
+          keyberon-firmware-stm32f4-elf = let
+            target = "thumbv7em-none-eabihf";
+          in
+            (naersk.lib.${system}.override {
+              cargo = toolchain;
+              rustc = toolchain;
+            })
+              .buildPackage {
+                src = ./.;
+                overrideMain = x: {
+                  preConfigure = ''
                 cargo_build_options="$cargo_build_options --package=keyboard-labs-keyberon-stm32f4"
               '';
-            };
-            CARGO_BUILD_TARGET = target;
-            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
-          };
+                };
+                CARGO_BUILD_TARGET = target;
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
+              };
 
-        keyberon-firmware-bin = pkgs.runCommand "keyboard-labs-keyberon-firmware-bin" {} ''
+          keyberon-firmware-bin = pkgs.runCommand "keyboard-labs-keyberon-firmware-bin" {} ''
           mkdir -p $out/bin
           export RUSTC=${toolchain}/bin/rustc
 
@@ -117,7 +121,7 @@
               ${nixpkgs.lib.concatStringsSep " " (map (x: "$out/bin/${x}.bin") stm32f4-bins)}
         '';
 
-        keyberon-firmware-uf2 = pkgs.runCommand "" {} ''
+          keyberon-firmware-uf2 = pkgs.runCommand "" {} ''
           mkdir -p $out/bin
 
           cp ${keyberon-firmware-bin}/bin/*.bin $out/bin
@@ -133,6 +137,7 @@
 
           rm $out/bin/*.bin
         '';
+        };
       };
-    });
+    };
 }
