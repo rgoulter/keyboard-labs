d3e77fee360b124d1c18e9c24fc161a86f6adc3f
Richard Goulter <richard.goulter@gmail.com>
2025-10-04 14:35:56 +0700

    flake.nix: remove qmk-, pcb- packages

--- flake.nix
+++ flake.nix
@@ -106,32 +106,26 @@
           };
         };
 
-        packages = let
-          bootloaders = import ./nix/pkgs/bootloaders {
-            inherit pkgs;
-          };
-          qmk =
-            lib.attrsets.mapAttrs' (name: p: lib.attrsets.nameValuePair "qmk-${name}" p)
-            (lib.attrsets.filterAttrs (_: p: lib.attrsets.isDerivation p) (pkgs.callPackage ./nix/pkgs/qmk {}));
-          pcb =
-            lib.attrsets.mapAttrs' (name: p: lib.attrsets.nameValuePair "pcb-${name}" p)
-            (lib.attrsets.filterAttrs (_: p: lib.attrsets.isDerivation p) (pkgs.callPackage ./pcb {}));
-        in
-          {
-            bootloader-stm32f103-stm32duino = bootloaders.stm32duino.stm32f103;
-            bootloader-stm32f401-tinyuf2 = bootloaders.tinyuf2.stm32f401;
-            bootloader-stm32f411-tinyuf2 = bootloaders.tinyuf2.stm32f411;
-
-            uf2conv = pkgs.callPackage ./nix/pkgs/uf2conv {};
-            wchisp = pkgs.callPackage ./nix/pkgs/wchisp {};
-
-            docker-kibot-kicad = import ./scripts/docker-kibot.nix {
-              pkgs = pkgs;
-              tag = "kicad-7";
+        packages =
+          let
+            bootloaders = import ./nix/pkgs/bootloaders {
+              inherit pkgs;
             };
-          }
-          // pcb
-          // qmk;
+          in
+            {
+              bootloader-stm32f103-stm32duino = bootloaders.stm32duino.stm32f103;
+              bootloader-stm32f401-tinyuf2 = bootloaders.tinyuf2.stm32f401;
+              bootloader-stm32f411-tinyuf2 = bootloaders.tinyuf2.stm32f411;
+
+              uf2conv = pkgs.callPackage ./nix/pkgs/uf2conv {};
+              wchisp = pkgs.callPackage ./nix/pkgs/wchisp {};
+
+              docker-kibot-kicad = import ./scripts/docker-kibot.nix {
+                pkgs = pkgs;
+                tag = "kicad-7";
+              };
+            }
+            ;
       };
     };
 }
