001fd68bde3e273073b8ecc5b938db2845dae591
Richard Goulter <richard.goulter@gmail.com>
2025-10-04 14:33:56 +0700

    flake.nix: remove checks.pcb

--- flake.nix
+++ flake.nix
@@ -122,22 +122,6 @@
             ];
           };
 
-          pcb = pkgs.symlinkJoin {
-            name = "keyboard-labs-pcb";
-            paths = [
-              self.packages.${system}.pcb-keyboard-100x100-minif4-dual-rgb-reversible
-              self.packages.${system}.pcb-keyboard-ch552-36-lhs
-              self.packages.${system}.pcb-keyboard-ch552-36-rhs
-              self.packages.${system}.pcb-keyboard-ch552-44
-              self.packages.${system}.pcb-keyboard-ch552-48
-              self.packages.${system}.pcb-keyboard-ch552-48-lpr
-              self.packages.${system}.pcb-keyboard-pico42
-              self.packages.${system}.pcb-keyboard-pykey40-hsrgb
-              self.packages.${system}.pcb-keyboard-pykey40-lite
-              self.packages.${system}.pcb-keyboard-x2-lumberjack-arm-hsrgb
-              self.packages.${system}.pcb-keyboard-x2-lumberjack-arm
-            ];
-          };
         };
 
         devenv.shells.default = {pkgs, ...}: {
